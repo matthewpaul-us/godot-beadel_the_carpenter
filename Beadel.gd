75160e610e319bb7983c6c2d321b6dce5db5f8fe
Matthew Paul <mpaul0416@gmail.com>
2020-05-07 23:34:42 -0400

    Add animation for the beetle

--- Beadel.gd
+++ Beadel.gd
@@ -7,48 +7,51 @@ export(float) var eat_speed := 75.0
 export(float) var move_speed := 200.0
 export(float) var turn_speed := 3.0
 
+onready var _anim := $AnimationPlayer
+
 var beadel_head
 var _velocity := Vector2.ZERO
 var _was_eating := false
 
+
 func _physics_process(delta):
-	if Input.is_action_pressed("eat"):
-		if not _was_eating:
-			emit_signal("eating_started")
-			_was_eating = true
-
-		_velocity = _forward() * eat_speed
-		if beadel_head:
-			beadel_head.show()
-	else:
-		if _was_eating:
-			emit_signal("eating_finished")
-			_was_eating = false
-
-		_velocity = Vector2.ZERO
-		if beadel_head:
-			beadel_head.hide()
-
-
-
-		# Don't let them go forward/backward, it'll mess up the eating
-		if Input.is_action_pressed("go_forward"):
-			_velocity += _forward() * move_speed
-
-		if Input.is_action_pressed("go_backward"):
-			_velocity -= _forward() * move_speed
-
-	if Input.is_action_pressed("turn_right"):
-		rotate(turn_speed * delta)
-
-	if Input.is_action_pressed("turn_left"):
-		rotate(-turn_speed * delta)
+#	if Input.is_action_pressed("eat"):
+#		if not _was_eating:
+#			emit_signal("eating_started")
+#			_was_eating = true
+#
+#		_velocity = _forward() * eat_speed
+#
+#	else:
+#		if _was_eating:
+#			emit_signal("eating_finished")
+#			_was_eating = false
+#
+#		_velocity = Vector2.ZERO
+#		if beadel_head:
+#			beadel_head.hide()
 
 	move_and_collide(_velocity * delta)
 
 	if beadel_head:
 		beadel_head.global_position = $BeetleHead.global_position
 		beadel_head.global_rotation = $BeetleHead.global_rotation
+
+func walk():
+	_anim.play("walk")
+
+func eat():
+	_anim.play("eat")
+	if beadel_head:
+			beadel_head.show()
+
+func stop_eat():
+	if beadel_head:
+			beadel_head.hide()
+
+func stop_animation():
+	_anim.stop()
+
 
 func _forward():
 	return Vector2(0, -1).rotated(rotation).normalized()
---
+++ beetle/BeetleFSM.gd
@@ -0,0 +1,74 @@
+extends "res://assets/scripts/StateMachine.gd"
+
+func _ready():
+	add_state("idle")
+	add_state("walking_forward")
+	add_state("walking_backward")
+	add_state("eating")
+	call_deferred('set_state', 'idle')
+
+func process_state(delta):
+	# We always want to be able to turn
+	if Input.is_action_pressed("turn_right"):
+		parent.rotate(parent.turn_speed * delta)
+
+	if Input.is_action_pressed("turn_left"):
+		parent.rotate(-parent.turn_speed * delta)
+
+	match state:
+		"eating":
+			parent._velocity = parent._forward() * parent.eat_speed
+
+		"walking_forward":
+			parent._velocity = parent._forward() * parent.move_speed
+
+		"walking_backward":
+			parent._velocity = -parent._forward() * parent.move_speed
+	pass
+
+func process_transition(delta):
+	match state:
+		"idle":
+			if Input.is_action_pressed("eat"):
+				return "eating"
+
+			if Input.is_action_pressed("go_forward"):
+				return "walking_forward"
+
+			if Input.is_action_pressed("go_backward"):
+				return "walking_backward"
+
+		"walking_forward":
+			if Input.is_action_pressed("eat"):
+				return "eating"
+			if not Input.is_action_pressed("go_forward"):
+				return "idle"
+
+		"walking_backward":
+			if Input.is_action_pressed("eat"):
+				return "eating"
+			if not Input.is_action_pressed("go_backward"):
+				return "idle"
+
+		"eating":
+			if not Input.is_action_pressed("eat"):
+				return "idle"
+
+	return null
+
+func enter_state(new_state, old_state):
+	if new_state == "walking_forward" or new_state == "walking_backward":
+		parent.walk()
+
+	if new_state == "eating":
+		parent.emit_signal("eating_started")
+		parent.eat()
+
+	if new_state == "idle":
+		parent._velocity = Vector2.ZERO
+		parent.stop_animation()
+
+func exit_state(old_state, new_state):
+	if old_state == "eating":
+		parent.emit_signal("eating_finished")
+		parent.stop_eat()
